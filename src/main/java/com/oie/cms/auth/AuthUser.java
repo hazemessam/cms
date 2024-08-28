119c72e067740c7ba670032c585e8029954bd03f
Hazem Essam <hazemkwita123@gmail.com>
2024-08-28 19:42:05 +0300

    Refactor

--- src/main/java/com/oie/cms/auth/AuthUser.java
+++ src/main/java/com/oie/cms/auth/AuthUser.java
@@ -32,5 +32,7 @@ public class AuthUser implements UserDetails {
         return emp;
     }
 
-    public EmployeeRole getRole() { return emp.getRole(); }
+    public EmployeeRole getRole() {
+        return emp.getRole();
+    }
 }
